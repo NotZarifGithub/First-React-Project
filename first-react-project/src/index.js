933b2bdf4f89beeb00847314a52af6d5d6fa08d8
NotZarifGithub <flacko.programming@gmail.com>
2023-01-19 07:13:08 +0800

    1st phase

--- first-react-project/src/index.js
+++ first-react-project/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Card from "./Card";
+import "tachyons";
+import {tools} from "./tools";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Card id={tools[0].id} name={tools[0].name} link={tools[0].link}/>
+      <Card id={tools[1].id} name={tools[1].name} link={tools[1].link}/>
+      <Card id={tools[2].id} name={tools[2].name} link={tools[2].link}/>
+      <Card id={tools[3].id} name={tools[3].name} link={tools[3].link}/>
+      <Card id={tools[4].id} name={tools[4].name} link={tools[4].link}/>
+      <Card id={tools[5].id} name={tools[5].name} link={tools[5].link}/>
   </React.StrictMode>
 );
 
